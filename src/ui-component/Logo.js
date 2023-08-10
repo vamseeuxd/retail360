01448a35385e014c6a06e24bc4c8ce1fdf0c32ab
vamseeuxd <vamsee.uxd@gmail.com>
2023-08-10 16:10:38 +0530

    init

---
+++ src/ui-component/Logo.js
@@ -0,0 +1,6 @@
+// import logo from 'assets/images/retail360logo.png';
+
+// const Logo = () => <img src={logo} alt="Berry" width="190" />;
+const Logo = () => <h1 className="logo-font">retail-360</h1>;
+
+export default Logo;
